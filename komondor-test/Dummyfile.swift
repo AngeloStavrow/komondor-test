18ef30d6a6222cb039a60c3859a9400becba7052
Angelo Stavrow <contact@angelostavrow.com>
2019-07-05 18:55:57 -0400

    Add dummy Swift file

---
+++ komondor-test/Dummyfile.swift
@@ -0,0 +1,11 @@
+//
+//  Dummyfile.swift
+//  komondor-test
+//
+//  Created by Angelo Stavrow on 2019-07-05.
+//
+
+import Foundation
+
+// This is just an arbitrary Swift file in the app, that has no dependencies
+// outside of Foundation.
