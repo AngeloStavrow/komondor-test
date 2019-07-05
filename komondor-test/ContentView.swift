a5b5fb7bdba238b66c4d243abc9b6cdf9cae84d5
Angelo Stavrow <contact@angelostavrow.com>
2019-07-05 18:52:19 -0400

    Initial Commit

---
+++ komondor-test/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  komondor-test
+//
+//  Created by Angelo Stavrow on 2019-07-05.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
